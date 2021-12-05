b914a5c33f3dbcf611d8600899e89b82c4bb291c
Takács Balázs <tbalazs.levente@gmail.com>
2021-12-05 15:19:32 +0100

    Végeredmény kerekítés

--- src/main/java/com/example/masodfoku/HelloController.java
+++ src/main/java/com/example/masodfoku/HelloController.java
@@ -18,29 +18,22 @@ public class HelloController {
     private Button btnSzamol;
 
     public void clickSzamol() {
-        try {
-            int a = bevitelA.getValue();
-            int b = bevitelB.getValue();
-            int c = bevitelC.getValue();
+        int a = bevitelA.getValue();
+        int b = bevitelB.getValue();
+        int c = bevitelC.getValue();
 
-            int d = (int) Math.pow(b, 2) - 4 * a * c;
+        int d = (int) Math.pow(b, 2) - 4 * a * c;
 
-            b *= -1;
-            double eredmeny = (b + Math.sqrt(d)) / (2 * a);
+        b *= -1;
+        double eredmeny = (b + Math.sqrt(d)) / (2 * a);
 
-            if (d < 0) {
-                lblEredmeny.setText("Nem oldható meg a valós számok halmazán");
-            } else if (d == 0) {
-                lblEredmeny.setText("x1 = x2 = " + eredmeny);
-            } else {
-                double eredmeny2 = (b - Math.sqrt(d)) / (2 * a);
-                lblEredmeny.setText("x1 = " + eredmeny + "\n" + "x2 = " + eredmeny2);
-            }
-        } catch (NumberFormatException e) {
-            Alert hibauzenet = new Alert(Alert.AlertType.NONE);
-            hibauzenet.setTitle("Hiba");
-            hibauzenet.setContentText("Számot adjon meg!");
-            hibauzenet.getButtonTypes().add(ButtonType.CLOSE);
+        if (d < 0) {
+            lblEredmeny.setText("Nem oldható meg a valós számok halmazán");
+        } else if (d == 0) {
+            lblEredmeny.setText("x1 = x2 = " + eredmeny);
+        } else {
+            double eredmeny2 = (b - Math.sqrt(d)) / (2 * a);
+            lblEredmeny.setText(String.format("x1 = %.2f\n x2 = %.2f", eredmeny, eredmeny2));
         }
     }
 }
